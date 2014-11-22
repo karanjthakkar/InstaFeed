61784be0c4d908e0bfad02bcf40e7ec8a7ffb44a
Karan Thakkar <karanjthakkar@gmail.com>
2014-11-22 14:37:43 +0530

    Added express. Check default instagram node api.

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var api = require('instagram-node').instagram();
+var app = express();
+
+api.use({
+  client_id: 01aa43eeb22142ed8ea8819adfaee95a,
+  client_secret: 02408ca932d844f194ddbb2b5c9783ae
+});
+
+var redirect_uri = 'http://ec2-54-148-28-246.us-west-2.compute.amazonaws.com:1337/';
+
+exports.authorize_user = function(req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+};
+
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+    if (err) {
+      console.log(err.body);
+      res.send("Didn't work");
+    } else {
+      console.log('Yay! Access token is ' + result.access_token);
+      res.send('You made it!!');
+    }
+  });
+};
+
+// This is where you would initially send users to authorize
+app.get('/authorize_user', exports.authorize_user);
+// This is your redirect URI
+app.get('/handleauth', exports.handleauth);
+
+app.listen(1337, function(){
+  console.log("Express server listening on port " + 1337);
+});
