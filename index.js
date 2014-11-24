bc3a41326b937d537299dfe908a14f10c7ca01cb
Karan Thakkar <karanjthakkar@gmail.com>
2014-11-25 02:35:59 +0530

    Testing mongo

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 var express = require('express');
 var api = require('instagram-node').instagram();
 var app = express();
+var mongoDao = require('./MongoClient');
 
 api.use({
   client_id: '01aa43eeb22142ed8ea8819adfaee95a',
   client_secret: '02408ca932d844f194ddbb2b5c9783ae'
 });
 
-var redirect_uri = 'http://ec2-54-148-28-246.us-west-2.compute.amazonaws.com:1337/';
+var redirect_uri = 'http://localhost:1337/handleauth';
+//var redirect_uri = 'http://ec2-54-148-28-246.us-west-2.compute.amazonaws.com:1337/';
 
-exports.authorize_user = function(req, res) {
-  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+exports.authorize_user = function (req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri, {
+    scope: ['basic', 'comments', 'relationships', 'likes']
+  }));
 };
 
-exports.handleauth = function(req, res) {
-  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+exports.handleauth = function (req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function (err, result) {
     if (err) {
       console.log(err.body);
-      res.send("Didn't work");
+      res.send('Didn\'t work');
     } else {
-      console.log('Yay! Access token is ' + result.access_token);
+      console.log('Yay! Access token is ', result);
       res.send('You made it!!');
     }
   });
 };
 
+exports.testConnect = function (req, res) {
+  mongoDao.connectToDb();
+  res.send('Connected');
+}
+
 // This is where you would initially send users to authorize
 app.get('/authorize_user', exports.authorize_user);
+
 // This is your redirect URI
 app.get('/handleauth', exports.handleauth);
 
-app.listen(1337, function(){
-  console.log("Express server listening on port " + 1337);
+app.listen(1337, function () {
+  console.log('Express server listening on port ' + 1337);
 });
