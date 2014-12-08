5f856e7d8e15fc5aac75b463c453eba26bd1558c
Karan Thakkar <karanjthakkar@gmail.com>
2014-12-08 16:48:05 +0530

    New Api's addded

--- index.js
+++ index.js
@@ -1,48 +1,38 @@
-var express = require('express');
-var api = require('instagram-node').instagram();
+'use strict';
+
+var fs = require('fs'),
+  path = require('path'),
+  express = require('express'),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser');
+
+/**
+ * Main application file
+ */
+
+// Connect to database
+mongoose.connect('mongodb://ec2-54-148-28-246.us-west-2.compute.amazonaws.com:27017/instafeed');
+
 var app = express();
-var mongoDao = require('./MongoClient');
 
-api.use({
-  client_id: '01aa43eeb22142ed8ea8819adfaee95a',
-  client_secret: '02408ca932d844f194ddbb2b5c9783ae'
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+// Bootstrap models
+var modelsPath = path.join(__dirname, 'lib/models');
+fs.readdirSync(modelsPath).forEach(function (file) {
+  require(modelsPath + '/' + file);
 });
 
-var redirect_uri = 'http://localhost:1337/handleauth';
-//var redirect_uri = 'http://ec2-54-148-28-246.us-west-2.compute.amazonaws.com:1337/';
+// Routing
+require('./lib/routes')(app);
 
-exports.authorize_user = function (req, res) {
-  res.redirect(api.get_authorization_url(redirect_uri, {
-    scope: ['basic', 'comments', 'relationships', 'likes']
-  }));
-};
-
-exports.handleauth = function (req, res) {
-  api.authorize_user(req.query.code, redirect_uri, function (err, result) {
-    if (err) {
-      console.log(err.body);
-      res.send('Didn\'t work');
-    } else {
-      console.log('Yay! Access token is ', result);
-      res.send('You made it!!');
-    }
-  });
-};
-
-exports.testConnect = function (req, res) {
-  mongoDao.connectToDb();
-  res.send('Connected');
-}
-
-// This is where you would initially send users to authorize
-app.get('/authorize_user', exports.authorize_user);
-
-// This is your redirect URI
-app.get('/handleauth', exports.handleauth);
-
-// This is your redirect URI
-app.get('/testconnect', exports.testConnect);
-
+// Start server
 app.listen(1337, function () {
-  console.log('Express server listening on port ' + 1337);
+  console.log('Express server listening on port 1337');
 });
+
+// Expose app
+exports = module.exports = app;
